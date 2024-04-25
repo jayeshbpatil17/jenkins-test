c22773563b6106cdac4d42a042582b4b74408151
Jayesh Patil <jayeshbpatil17@gmail.com>
2024-04-26 01:03:06 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is java program");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+        System.out.println("hello");
+        System.out.println("Added one more file");
+        Student student = new Student();
+        student.sayHello();
+    }
+
+}
